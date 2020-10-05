9a97dd39ee6ce8b4f3a2f57d1318674945b83662
Chris on Code <hey@chrisoncode.io>
2020-10-05 07:27:23 -0700

    donezo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,40 @@
 import React from "react"
+import "normalize.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div
+      style={{
+        fontFamily: "Arial, sans-serif",
+        minHeight: "100vh",
+        background: "#663399",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h2
+        style={{
+          fontFamily: "Arial, sans-serif",
+          color: "white",
+          fontSize: "40px",
+          marginBottom: "8px",
+        }}
+      >
+        Hello from Gatsby!
+      </h2>
+      <h3
+        style={{
+          fontFamily: "Arial, sans-serif",
+          color: "#fff",
+          opacity: "0.25",
+          fontSize: "18px",
+          marginTop: "0",
+        }}
+      >
+        Deployed to DigitalOcean
+      </h3>
+    </div>
+  )
 }
